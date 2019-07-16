3aa7734727921687ae959951c2d92b0388bd0ea5
Michael Chen <michaelchen@capeanalytics.com>
2019-07-15 22:10:37 -0700

    change to static caller.

--- pywebsocketgame/__init__.py
+++ pywebsocketgame/__init__.py
@@ -1,15 +1,12 @@
 from websocket import create_connection
 
-ws = None
-
 
 def join_game():
-    ws = create_connection("wss://p0d0cvu6q0.execute-api.us-east-1.amazonaws.com/dev/")
-    return ws
+    return create_connection("wss://p0d0cvu6q0.execute-api.us-east-1.amazonaws.com/dev/")
 
 
-def move_forward():
-    if ws is None:
+def move_forward(connection):
+    if connection is None:
         print("please call join_game first")
         return
-    ws.send('{"action": "sendMessage", "name": "mike2", "channelId": "General", "content": "hello world3!"}')
+    connection.send('{"action": "sendMessage", "name": "mike2", "channelId": "General", "content": "hello world3!"}')
