6636ed539ca61cd01dbd12efa4dd3fcdc1cb09eb
Michael Chen <michaelchen@capeanalytics.com>
2019-07-16 06:53:46 -0700

    update content.

--- pywebsocketgame/__init__.py
+++ pywebsocketgame/__init__.py
@@ -1,12 +1,33 @@
 from websocket import create_connection
+import json
 
 
-def join_game():
-    return create_connection("wss://p0d0cvu6q0.execute-api.us-east-1.amazonaws.com/dev/")
+def join_game(name):
+    connection = create_connection("wss://p0d0cvu6q0.execute-api.us-east-1.amazonaws.com/dev/")
+    connection.send('{"action": "sendMessage", "name": "{}", "channelId": "General", "content": "join_game"}'.format(name))
+    return connection
 
 
-def move_forward(connection):
-    if connection is None:
-        print("please call join_game first")
-        return
-    connection.send('{"action": "sendMessage", "name": "mike2", "channelId": "General", "content": "hello world3!"}')
+def forward(connection):
+    connection.send('{"action": "sendMessage", "name": "name", "channelId": "General", "content": "forward"}')
+
+
+def stop(connection):
+    connection.send('{"action": "sendMessage", "name": "name", "channelId": "General", "content": "stop"}')
+
+
+def find_giphymon(connection):
+    connection.send('{"action": "sendMessage", "name": "name", "channelId": "General", "content": "find_giphymon"}')
+
+
+def close_to_giphymon(connection):
+    connection.send('{"action": "sendMessage", "name": "name", "channelId": "General", "content": "close_to_giphymon"}')
+    while True:
+        result = connection.recv()
+        result = json.loads(result)
+        if 'filterId' in result and result['name'] == 'closetogiphymonresponse':
+            return result['content']
+
+
+def capture_giphymon(connection):
+    connection.send('{"action": "sendMessage", "name": "name", "channelId": "General", "content": "capture_giphymon"}')
